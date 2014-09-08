37126e89fcab94869b31c7b8dccb22cf85f255e7
matt-softlogic <matt@soft-logic.net>
2014-09-08 14:53:18 -0700

    Pre Hangfire bit

---
+++ Hangfire.Highlighter/Models/HighlighterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hangfire.Highlighter.Models
+{
+    public class HighlighterDbContext : DbContext
+    {
+        public HighlighterDbContext() : base("HighlighterDb")
+        {
+        }
+
+        // ~/Models/HighlighterDbContext.cs
+        public DbSet<CodeSnippet> CodeSnippets { get; set; }
+    }
+}
